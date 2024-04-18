57a31ab52d1292c6e1ce053bc3d5a82cb0817d85
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-18 08:13:47 -0700

    wip importing gander test code

---
+++ test/FileNameTest.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module FileNameTest where
+
+import System.Directory.BigTrees.HashLine
+import Test.QuickCheck
+import Test.QuickCheck.Instances
+import System.Directory.BigTrees.Util
+
+import qualified Data.Attoparsec.ByteString.Char8 as A8
+import qualified Data.ByteString.Char8            as B
+import qualified Data.Text                        as T
+
+import System.IO               (hClose)
+import System.IO.Temp          (withSystemTempDirectory)
+import Test.QuickCheck.Monadic (run, assert, pick, monadicIO)
+import Test.QuickCheck.Unicode (list, char)
+import System.FilePath ((</>))
+
+import qualified Filesystem.Path.CurrentOS as OS
+
+{- My `FileName` type is defined in `Util` as `Text` for efficiency, but
+ - what it really means is "Text without slashes or null chars". So I have to
+ - define my own Arbitrary instance here.
+ -
+ - TODO why is the not . null thing required to prevent empty strings? list1 should be enough
+ - TODO wait, is the empty string also a valid filename?
+ -}
+instance Arbitrary FileName where
+  arbitrary = FileName <$> (arbitrary :: Gen T.Text) `suchThat` validFileName
+  shrink (FileName t) = FileName <$> filter validFileName (shrink t)
+
+validFileName :: T.Text -> Bool
+validFileName t = not (t `elem` ["", ".", ".."])
+               && (not . T.any (== '/')) t -- no separators
+               && (OS.valid . OS.fromText) t
+
+prop_roundtrip_filename_to_bytestring :: FileName -> Bool
+prop_roundtrip_filename_to_bytestring n = p2n (n2p n) == n
+
+roundtrip_filename_to_name_of_tmpfile :: FileName -> IO ()
+roundtrip_filename_to_name_of_tmpfile n = withSystemTempDirectory "roundtriptemp" $ \d -> do
+  let f = d </> n2p n
+  B.writeFile f "this is a test"
+  _ <- B.readFile f
+  return ()
+
+prop_roundtrip_filename_to_name_of_tmpfile :: Property
+prop_roundtrip_filename_to_name_of_tmpfile = monadicIO $ do
+  n <- pick arbitrary
+  run $ roundtrip_filename_to_name_of_tmpfile n
+  assert True
---
+++ test/UtilTest.hs
@@ -0,0 +1,80 @@
+module UtilTest where
+
+import System.Directory.BigTrees.Util
+import FileNameTest
+
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Test.HUnit (Assertion, (@=?))
+import Test.Hspec
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>), takeDirectory, joinPath)
+import Control.Monad.IO.Class (liftIO)
+import Control.Exception (evaluate)
+import qualified Data.Text as T
+
+-- describe "Util" $ do
+--   describe "absolutize" $ do
+--       it "strips dots from paths" pending
+--       it "does not follow symlinks" pending
+-- 
+--     describe "findAnnex" $ do
+--       it "returns Nothing if given a nonexistent path" pending
+--       it "returns Nothing if given a non-annex path" pending
+--       it "returns (Just path) if path is an annex" pending
+--       it "returns only absolute paths" pending
+-- 
+--     describe "isAnnexSymlink" $ do
+--       it "returns False if given a non-symlink" pending
+--       it "returns True if given a symlink pointing into a .git/annex/objects dir" pending
+--       it "returns False if given a symlink pointing somewhere else" pending
+-- 
+--     describe "isNonAnnexSymlink" $ do
+--       it "returns False if given a non-symlink" pending
+--       it "returns False if given a symlink pointing into a .git/annex/objects dir" pending
+--       it "returns True if given a symlink pointing somewhere else" pending
+
+newtype ValidFilePath = ValidFilePath FilePath
+  deriving (Eq, Ord, Read, Show)
+
+instance Arbitrary ValidFilePath where
+  arbitrary = do
+    prefix <- oneof $ map pure ["", ".", "..", "~"]
+    comps  <- (fmap . map) (\(FileName t) -> T.unpack t) $ listOf $ (arbitrary :: Gen FileName)
+    let path = joinPath (prefix:comps)
+    return $ ValidFilePath $ if null path then "/" else path
+
+unit_absolutize_expands_tildes :: Assertion
+unit_absolutize_expands_tildes = do
+  home <- getHomeDirectory
+  let explicit = home </> "xyz"
+  (Just implicit) <- absolutize "~/xyz"
+  implicit @=? explicit
+
+unit_absolutize_rejects_the_null_path :: Assertion
+unit_absolutize_rejects_the_null_path = do
+  reject <- absolutize ""
+  reject @=? Nothing
+
+unit_absolutize_fixes_invalid_dotdot_path :: Assertion
+unit_absolutize_fixes_invalid_dotdot_path = do
+  fixed <- absolutize "/.." -- one level above / is invalid
+  fixed @=? Just "/"
+
+prop_absolutize_is_idempotent :: ValidFilePath -> Property
+prop_absolutize_is_idempotent (ValidFilePath path) = monadicIO $ do
+  (Just path' ) <- liftIO $ absolutize path
+  (Just path'') <- liftIO $ absolutize path'
+  assert $ path' == path''
+
+prop_absolutize_strips_redundant_dotdot :: ValidFilePath -> Property
+prop_absolutize_strips_redundant_dotdot (ValidFilePath path) = monadicIO $ do
+  (Just abs ) <- fmap (fmap takeDirectory) $ liftIO $ absolutize path
+  (Just abs') <- liftIO $ absolutize $ path </> ".."
+  assert $ abs == abs'
+
+prop_absolutize_strips_redundant_dot :: ValidFilePath -> Property
+prop_absolutize_strips_redundant_dot (ValidFilePath path) = monadicIO $ do
+  (Just abs ) <- liftIO $ absolutize path
+  (Just abs') <- liftIO $ absolutize $ path </> "."
+  assert $ abs == abs'
