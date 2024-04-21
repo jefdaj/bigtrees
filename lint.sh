fc26eab85f6af973dd0b2b3d062eef3d462a7158
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 09:41:02 -0700

    add default hlint.yml

--- lint.sh
+++ lint.sh
@@ -10,9 +10,8 @@ rm -rf .stack-work/
 stack build | tee -a "$LOG"
 
 find * -name '*.hs' | while read hs; do
-  # TODO can this take a config file?
   # TODO can it be auto-accepted? the prompts don't show through tee
-  hlint "$hs" --refactor --refactor-options="-i -s" | tee -a "$LOG"
+  hlint --hint .lint/hlint.yml "$hs" --refactor --refactor-options="-i -s" | tee -a "$LOG"
 done
 
 stylish-haskell --config .lint/stylish-haskell.yaml -r -i . | tee -a "$LOG"
