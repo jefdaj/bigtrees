b9669eb38c5c1a25da487e03f1f45fba0edc0aa5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 09:31:08 -0700

    wip lint.sh

--- lint.sh
+++ lint.sh
@@ -2,18 +2,26 @@
 
 set -x
 
+LOG='lint.log'
+
+rm -f "$LOG"
+rm -rf .stack-work/
+
+stack build | tee -a "$LOG"
+
 find * -name '*.hs' | while read hs; do
   # TODO can this take a config file?
-  hlint "$hs" --refactor --refactor-options="-i -s"
+  # TODO can it be auto-accepted? the prompts don't show through tee
+  hlint "$hs" --refactor --refactor-options="-i -s" | tee -a "$LOG"
 done
 
-stylish-haskell --config .lint/stylish-haskell.yaml -r -i .
+stylish-haskell --config .lint/stylish-haskell.yaml -r -i . | tee -a "$LOG"
 
 # TODO add to ignores instead?
 git checkout lib/System/Directory/Tree.hs
 
-weeder --config .lint/weeder.toml
+weeder --config .lint/weeder.toml | tee -a "$LOG"
 
-stack test
+stack test | tee -a "$LOG"
 
-git status
+git status | tee -a "$LOG"
