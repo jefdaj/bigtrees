120790c664f7377e9c8822063f8ffebfbf30ec77
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 09:45:10 -0700

    lint.sh capture stderr to log

--- lint.sh
+++ lint.sh
@@ -7,20 +7,20 @@ LOG='lint.log'
 rm -f "$LOG"
 rm -rf .stack-work/
 
-stack build | tee -a "$LOG"
+stack build 2>&1 | tee -a "$LOG"
 
 find * -name '*.hs' | while read hs; do
   # TODO can it be auto-accepted? the prompts don't show through tee
-  hlint --hint .lint/hlint.yml "$hs" --refactor --refactor-options="-i -s" | tee -a "$LOG"
+  hlint --hint .lint/hlint.yml "$hs" --refactor --refactor-options="-i -s" 2>&1 | tee -a "$LOG"
 done
 
-stylish-haskell --config .lint/stylish-haskell.yaml -r -i . | tee -a "$LOG"
+stylish-haskell --config .lint/stylish-haskell.yaml -r -i . 2>&1 | tee -a "$LOG"
 
 # TODO add to ignores instead?
 git checkout lib/System/Directory/Tree.hs
 
-weeder --config .lint/weeder.toml | tee -a "$LOG"
+weeder --config .lint/weeder.toml 2>&1 | tee -a "$LOG"
 
-stack test | tee -a "$LOG"
+stack test 2>&1 | tee -a "$LOG"
 
-git status | tee -a "$LOG"
+git status 2>&1 | tee -a "$LOG"
