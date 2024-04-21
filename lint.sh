ae803ca0ce35a3934b53b86886563f291a417a9d
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 09:20:33 -0700

    lint config

--- lint.sh
+++ lint.sh
@@ -3,13 +3,16 @@
 set -x
 
 find * -name '*.hs' | while read hs; do
+  # TODO can this take a config file?
   hlint "$hs" --refactor --refactor-options="-i -s"
 done
 
-stylish-haskell -r -i lib/
+stylish-haskell --config .lint/stylish-haskell.yaml -r -i .
 
 # TODO add to ignores instead?
 git checkout lib/System/Directory/Tree.hs
+
+weeder --config .lint/weeder.toml
 
 stack test
 
