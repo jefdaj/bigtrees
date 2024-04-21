ca8081747a6572d0355bddd922f8f58593dca618
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 14:06:35 -0700

    write test_demo_diff, test_demo_dupes

--- app/Cmd/Diff.hs
+++ app/Cmd/Diff.hs
@@ -2,11 +2,49 @@ module Cmd.Diff where
 
 -- TODO guess and check hashes
 
-import Config (Config (..))
+import Config (Config (..), defaultConfig)
+import qualified Control.Concurrent.Thread.Delay as D
+import qualified Data.ByteString.Lazy.UTF8 as BLU
 import System.Directory.BigTrees (diff, printDeltas, readOrBuildTree, renameRoot)
+import System.Directory.BigTrees.Util (absolutize)
+import System.FilePath (dropExtension, takeBaseName, (</>))
+import System.IO (stderr, stdout)
+import System.IO.Silently (hCapture)
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (cwd, proc, readCreateProcess)
+import Test.Tasty (TestTree)
+import Test.Tasty.Golden (goldenVsString)
 
 cmdDiff :: Config -> FilePath -> FilePath -> IO ()
 cmdDiff cfg old new = do
   tree1 <- renameRoot "old" <$> readOrBuildTree (verbose cfg) (maxdepth cfg) (exclude cfg) old
   tree2 <- renameRoot "new" <$> readOrBuildTree (verbose cfg) (maxdepth cfg) (exclude cfg) new
   printDeltas $ diff tree1 tree2
+
+-----------
+-- tests --
+-----------
+
+diffTarXz :: FilePath -> FilePath -> IO BLU.ByteString
+diffTarXz xz1 xz2 = do
+  (Just xz1') <- absolutize xz1
+  (Just xz2') <- absolutize xz2
+  withSystemTempDirectory "/tmp/bigtrees" $ \tmpDir -> do
+    let d1 = tmpDir </> dropExtension (takeBaseName xz1')
+    let d2 = tmpDir </> dropExtension (takeBaseName xz2')
+    D.delay 100000 -- wait 0.1 second so we don't capture output from tasty
+    _ <- readCreateProcess ((proc "tar" ["-xf", xz1']) {cwd = Just tmpDir}) ""
+    _ <- readCreateProcess ((proc "tar" ["-xf", xz2']) {cwd = Just tmpDir}) ""
+    (out, ()) <- hCapture [stdout, stderr] $ cmdDiff defaultConfig d1 d2
+    D.delay 100000 -- wait 0.1 second so we don't capture output from tasty
+    return $ BLU.fromString out
+
+test_demo_diff :: TestTree
+test_demo_diff =
+  let xz1 = "test/app/demo1.tar.xz"
+      xz2 = "test/app/demo2.tar.xz"
+      gld = "test/app/demo12.diff"
+  in goldenVsString
+       "diff demo1 -> demo2"
+       gld
+       (diffTarXz xz1 xz2)
--- app/Cmd/Dupes.hs
+++ app/Cmd/Dupes.hs
@@ -2,8 +2,19 @@ module Cmd.Dupes where
 
 -- TODO guess and check hashes
 
-import Config (Config (..))
+import Config (Config (..), defaultConfig)
+import qualified Control.Concurrent.Thread.Delay as D
+import qualified Data.ByteString.Lazy.UTF8 as BLU
 import qualified System.Directory.BigTrees as BT
+import System.Directory.BigTrees (diff, printDeltas, readOrBuildTree, renameRoot)
+import System.Directory.BigTrees.Util (absolutize)
+import System.FilePath (dropExtension, takeBaseName, (</>))
+import System.IO (stderr, stdout)
+import System.IO.Silently (hCapture)
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (cwd, proc, readCreateProcess)
+import Test.Tasty (TestTree)
+import Test.Tasty.Golden (goldenVsString)
 
 cmdDupes :: Config -> [FilePath] -> IO ()
 cmdDupes cfg paths = do
@@ -15,3 +26,31 @@ cmdDupes cfg paths = do
   case txt cfg of
     Nothing -> BT.printDupes (maxdepth cfg) ds
     Just p  -> BT.writeDupes (maxdepth cfg) p ds
+
+-----------
+-- tests --
+-----------
+
+dupesTarXz :: FilePath -> FilePath -> IO BLU.ByteString
+dupesTarXz xz1 xz2 = do
+  (Just xz1') <- absolutize xz1
+  (Just xz2') <- absolutize xz2
+  withSystemTempDirectory "/tmp/bigtrees" $ \tmpDir -> do
+    let d1 = tmpDir </> dropExtension (takeBaseName xz1')
+    let d2 = tmpDir </> dropExtension (takeBaseName xz2')
+    D.delay 100000 -- wait 0.1 second so we don't capture output from tasty
+    _ <- readCreateProcess ((proc "tar" ["-xf", xz1']) {cwd = Just tmpDir}) ""
+    _ <- readCreateProcess ((proc "tar" ["-xf", xz2']) {cwd = Just tmpDir}) ""
+    (out, ()) <- hCapture [stdout, stderr] $ cmdDupes defaultConfig [d1, d2]
+    D.delay 100000 -- wait 0.1 second so we don't capture output from tasty
+    return $ BLU.fromString out
+
+test_demo_dupes :: TestTree
+test_demo_dupes =
+  let xz1 = "test/app/demo1.tar.xz"
+      xz2 = "test/app/demo2.tar.xz"
+      gld = "test/app/demo12.dupes"
+  in goldenVsString
+       "dupes demo1 + demo2"
+       gld
+       (dupesTarXz xz1 xz2)
