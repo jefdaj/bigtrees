2342b8fbe007a620df30089713e3c923fa41a2db
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-21 17:18:50 -0700

    mv .hie files back to .stack-work

--- lint.sh
+++ lint.sh
@@ -22,10 +22,11 @@ stylish-haskell --config stylish-haskell.yaml -r -i .. 2>&1 | tee -a "$LOG"
 # TODO add to ignores instead?
 git checkout ../lib/System/Directory/Tree.hs
 
-stan --hiedir ../.hie --config-file stan.toml report 2>&1 | tee -a "$LOG"
+stan --hiedir ../.stack-work --config-file stan.toml report 2>&1 | tee -a "$LOG"
 
 popd
 
+echo "weeder weeds:" | tee -a "$LOG"
 weeder --config .lint/weeder.toml 2>&1 | uniq | tee -a "$LOG"
 
 # tests that build works after any changes
