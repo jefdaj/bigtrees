faec565c2228cad9b662cf99f0c1e2ee097d6c7d
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-04-24 19:53:29 -0700

    Revert "mv .hie files back to .stack-work"
    This reverts commit 2342b8fbe007a620df30089713e3c923fa41a2db.

--- lint.sh
+++ lint.sh
@@ -23,11 +23,10 @@ stylish-haskell --config stylish-haskell.yaml -r -i .. 2>&1 | tee -a "$LOG"
 # TODO add to ignores instead?
 git checkout ../lib/System/Directory/Tree.hs
 
-stan --hiedir ../.stack-work --config-file stan.toml report 2>&1 | tee -a "$LOG"
+stan --hiedir ../.hie --config-file stan.toml report 2>&1 | tee -a "$LOG"
 
 popd
 
-echo "weeder weeds:" | tee -a "$LOG"
 weeder --config .lint/weeder.toml 2>&1 | uniq | tee -a "$LOG"
 
 # tests that build works after any changes
