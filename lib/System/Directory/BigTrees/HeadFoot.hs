c29a4de0a66778acd03b990e14050a943112905f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-05-24 21:03:35 -0700

    wip HeadFoot

--- lib/System/Directory/BigTrees/HeadFoot.hs
+++ lib/System/Directory/BigTrees/HeadFoot.hs
@@ -29,28 +29,43 @@ import GHC.Generics (Generic)
 
 -- TODO generalize so we can fit other things in the header too? or are explicit values enough?
 
+--- utilities ---
+
 apConf :: AP.Config
 apConf = AP.defConfig
   { AP.confIndent = AP.Spaces 2
   , AP.confTrailingNewline = True
   }
 
+now :: IO Integer
+now = getPOSIXTime >>= return . round
+
+commentLines :: [B8.ByteString] -> [B8.ByteString]
+commentLines = map (B8.append "# ")
+
+-- TODO proper time type for this?
+scanSeconds :: (Header, Footer) -> Integer
+scanSeconds (h, f) = scanEnd f - scanStart h
+
+treeInfo :: (Header, Footer) -> B8.ByteString
+treeInfo = undefined
+
+--- header ---
+
 data Header = Header
   { excludes  :: [String]
   , maxdepth  :: Maybe Int
-  , system    :: String   -- os, arch
-  , compiler  :: String   -- compiler, version
-  , bigtrees  :: String   -- format version string, from cabal file
-  -- , fields    :: [String] -- field order, hardcoded
+  , system    :: String -- os, arch TODO uname?
+  , compiler  :: String -- compiler, version
+  , bigtrees  :: String -- format version string, from cabal file
+  , scanFormat :: Int
   , scanStart :: Integer
+  -- TODO locale
   }
   deriving (Eq, Read, Show, Generic)
 
 instance ToJSON   Header
 instance FromJSON Header
-
-now :: IO Integer
-now = getPOSIXTime >>= return . round
 
 makeHeaderNow :: [String] -> Maybe Int -> IO Header
 makeHeaderNow es md = do
@@ -59,16 +74,13 @@ makeHeaderNow es md = do
   let header = Header
         { excludes  = es
         , maxdepth  = md
-        , system    = os ++ "-" ++ arch
-        , compiler  = compilerName ++ " " ++ showVersion fullCompilerVersion
+        , system    = unwords [os, arch]
+        , compiler  = unwords [compilerName, showVersion fullCompilerVersion]
         , bigtrees  = showVersion version
+        , scanFormat = 2 -- update when changing anything that breaks parser
         , scanStart = startTime
-        -- , fields    = intercalate "\t" hashLineFields
         }
   return header
-
-commentLines :: [B8.ByteString] -> [B8.ByteString]
-commentLines = map (B8.append "# ")
 
 renderHeader :: Header -> B8.ByteString
 renderHeader h = B8.unlines $ commentLines $ (B8.lines header) ++ [fields]
@@ -76,17 +88,19 @@ renderHeader h = B8.unlines $ commentLines $ (B8.lines header) ++ [fields]
     header = B8.toStrict $ AP.encodePretty' apConf h
     fields = join $ map B8.pack hashLineFields
 
+writeHeader :: Handle -> [String] -> Maybe Int -> IO ()
+writeHeader hdl es md = do
+  hdr <- makeHeaderNow es md
+  B8.hPutStrLn hdl $ renderHeader hdr
+
+--- footer ---
+
 data Footer = Footer
   { scanEnd    :: Integer
   , nSuccesses :: Int -- TODO integer?
   , nErrors    :: Int
   }
   deriving (Eq, Read, Show, Generic)
-
-renderFooter :: Footer -> B8.ByteString
-renderFooter f = B8.unlines $ commentLines $ B8.lines footer
-  where
-    footer = B8.toStrict $ AP.encodePretty' apConf f
 
 instance ToJSON   Footer
 instance FromJSON Footer
@@ -101,25 +115,12 @@ makeFooterNow (nOK, nErr) = do
         }
   return footer
 
--- TODO proper time type for this?
-scanSeconds :: (Header, Footer) -> Integer
-scanSeconds (h, f) = scanEnd f - scanStart h
+renderFooter :: Footer -> B8.ByteString
+renderFooter f = B8.unlines $ commentLines $ B8.lines footer
+  where
+    footer = B8.toStrict $ AP.encodePretty' apConf f
 
-treeInfo :: (Header, Footer) -> B8.ByteString
-treeInfo = undefined
-
-writeHeader :: Handle -> IO ()
-writeHeader h = do
-  -- TODO bigtrees: version
-  -- TODO system: os, arch, locale
-  -- TODO config: maxdepth, excludes
-  -- TODO scan: start time
-  -- TODO is filesystem easy?
-  -- hPrintf h $ ""
-  return ()
-
-writeFooter :: Handle -> IO ()
-writeFooter h = do
-  -- TODO n errors?
-  -- TODO end time
-  return ()
+writeFooter :: Handle -> (Int, Int) -> IO ()
+writeFooter hdl oe = do
+  ftr <- makeFooterNow oe
+  B8.hPutStrLn hdl $ renderFooter ftr
