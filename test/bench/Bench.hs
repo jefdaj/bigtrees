2a4f09a6c619e65e416229558f0722e7ddd3e76e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-08 09:19:16 -0700

    try moving a benchmark -> the lib

--- test/bench/Bench.hs
+++ test/bench/Bench.hs
@@ -1,15 +1,15 @@
 import Test.Tasty.Bench
-import Test.Tasty.QuickCheck -- from tasty-quickcheck package
+import Test.Tasty.QuickCheck
 
-fibo :: Int -> Integer
-fibo n = if n < 2 then toInteger n else fibo (n - 1) + fibo (n - 2)
+-- TODO get tasty-discover to pick up these bench_* functions
+import System.Directory.BigTrees.HashTree (bench_fibo)
 
 myFibo :: Int -> Integer
 myFibo n = if n < 3 then toInteger n else myFibo (n - 1) + myFibo (n - 2)
 
 main :: IO ()
 main = Test.Tasty.Bench.defaultMain -- not Test.Tasty.defaultMain
-  [ bench "fibo   20" $ nf fibo   20
-  , bench "myFibo 20" $ nf myFibo 20
-  , testProperty "myFibo = fibo" $ \n -> fibo n === myFibo n
+  [ bench "fibo   20" $ nf bench_fibo   20
+  -- , bench "myFibo 20" $ nf myFibo 20
+  -- , testProperty "myFibo = fibo" $ \n -> fibo n === myFibo n
   ]
