bd6fa6928bfee38f5757c3503c5256a6fa74d689
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-13 09:29:52 -0700

    wip round-trip dirs too

--- RoundTripNames.hs
+++ RoundTripNames.hs
@@ -15,6 +15,7 @@ import qualified System.OsPath.Internal as OSPI
 -- import qualified System.OsString as OSS
 import System.IO.Temp (withSystemTempDirectory)
 import qualified System.Directory.OsPath as SDO
+import Data.Maybe (fromJust)
 
 -- instance Arbitrary OSP.OsPath where
   -- arbitrary = <$> ((arbitrary :: Gen SBS.ShortByteString) `suchThat` isValidName)
@@ -32,30 +33,44 @@ main :: IO ()
 main = do
 
   -- generating them isn't hard
-  bs <- generate ((arbitrary :: Gen SBS.ShortByteString) `suchThat` isValidName)
-  putStrLn $ show bs
+  bs1 <- generate ((arbitrary :: Gen SBS.ShortByteString) `suchThat` isValidName)
+  bs2 <- generate ((arbitrary :: Gen SBS.ShortByteString) `suchThat` isValidName)
+  putStrLn $ show bs1
+  putStrLn $ show bs2
 
   -- converting to ospath isn't hard, as long as you're ok doing it outside parser monad?
-  op1 <- OSPI.fromBytes $ SBS.fromShort bs
+  op1 <- OSPI.fromBytes $ SBS.fromShort bs1
+  op2 <- OSPI.fromBytes $ SBS.fromShort bs2
   putStrLn $ show op1
+  putStrLn $ show op2
 
   -- TODO but instead can you just "cast" it directly with a hidden constructor?
   -- op2 = ...
 
   -- round-trip to filename
   withSystemTempDirectory "roundtripnames" $ \tmpDir -> do
-    tmpDir' <- OSP.encodeUtf tmpDir 
-    let tmpPath = tmpDir' OSP.</> op1
+    tmpDir' <- OSP.encodeUtf tmpDir
+    let tmpInner = tmpDir' OSP.</> op1
+    let tmpPath = tmpInner OSP.</> op2
     putStrLn $ show tmpPath
     let txt = "this is a test"
 
-    -- test: should be able to write the file
+    -- test: should be able to create the dir
+    SDO.createDirectoryIfMissing False tmpInner
+
+    -- test: should be able to see it in the outer tmpdir
+    cs1 <- SDO.getDirectoryContents tmpDir'
+    putStrLn $ "tmpdir present? " ++ show (op1 `elem` cs1)
+    putStrLn $ show cs1
+
+    -- test: should be able to write the file to the inner tmpdir
     SFO.writeFile tmpPath txt
 
-    -- test: should be able to find the file in the dir
-    cs <- SDO.getDirectoryContents tmpDir'
-    putStrLn $ "filename present? " ++ show (op1 `elem` cs)
-    putStrLn $ show cs
+    -- test: should be able to read dir contents of inner tmpdir
+    -- test: should be able to find the file in the inner dir
+    cs2 <- SDO.getDirectoryContents tmpInner
+    putStrLn $ "filename present? " ++ show (op2 `elem` cs2)
+    putStrLn $ show cs2
 
     -- test: should be able to read the file contents back
     txt' <- SFO.readFile tmpPath
