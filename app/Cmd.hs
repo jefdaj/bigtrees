22ae9c422989aac9f0b573642594db178ed584b7
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-15 09:28:39 -0700

    OldCmd

--- app/Cmd.hs
+++ app/Cmd.hs
@@ -1,8 +1,14 @@
 module Cmd
   ( cmdInfo
   , cmdFind
+  , cmdDiff
+  , cmdDupes
+  , cmdHash
   )
   where
 
 import Cmd.Find (cmdFind)
 import Cmd.Info (cmdInfo)
+import Cmd.Diff (cmdDiff)
+import Cmd.Dupes (cmdDupes)
+import Cmd.Hash (cmdHash)
--- app/OldCmd.hs
+++ app/OldCmd.hs
@@ -1,21 +1,18 @@
 module OldCmd
+
   -- ( oldCmdAdd
   -- , cmdDedup
-  ( oldCmdCat
-  , cmdDiff
-  , cmdDupes
-  , cmdHash
   -- , oldCmdInit
   -- , oldCmdMv
   -- , oldCmdRm
   -- , oldCmdTest
+
+  ( oldCmdCat
   , oldCmdUpdate
   )
   where
 
-import Cmd.Diff (cmdDiff)
-import Cmd.Dupes (cmdDupes)
-import Cmd.Hash (cmdHash)
-import OldCmd.Cat (oldCmdCat)
 -- import OldCmd.Test (oldCmdTest)
+
+import OldCmd.Cat (oldCmdCat)
 import OldCmd.Update (oldCmdUpdate)
