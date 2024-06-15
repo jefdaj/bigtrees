bfce467fda0999acd10714db6e3acbfba9b6125e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-15 09:23:43 -0700

    wip OldCmd.Update

--- app/OldCmd/Update.hs
+++ app/OldCmd/Update.hs
@@ -4,10 +4,11 @@ module OldCmd.Update where
 -- TODO make an operation to replace a subtree (insert despite it existing, then diff)
 
 import Config (Config (..))
-import System.Directory.BigTrees (addSubTree, printTree, readOrBuildTree)
+import System.Directory.BigTrees (addSubTree, printTree, readOrBuildTree, op2ns)
+import System.OsPath (OsPath)
 
-oldCmdUpdate :: Config -> FilePath -> FilePath -> FilePath -> IO ()
+oldCmdUpdate :: Config -> OsPath -> OsPath -> OsPath -> IO ()
 oldCmdUpdate cfg root sub path = do
   tree1 <- readOrBuildTree (verbose cfg) (maxdepth cfg) (exclude cfg) root
   tree2 <- readOrBuildTree (verbose cfg) (maxdepth cfg) (exclude cfg) sub
-  printTree $ addSubTree tree1 tree2 path
+  printTree $ addSubTree tree1 tree2 $ op2ns path
