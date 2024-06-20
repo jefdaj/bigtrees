b3732252a9e4f26aefdbf8bf7a89d939951bba4f
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-20 11:33:22 -0700

    wip start Prune.hs

--- lib/System/Directory/BigTrees/HashTree.hs
+++ lib/System/Directory/BigTrees/HashTree.hs
@@ -75,6 +75,7 @@ import System.Directory.BigTrees.HashTree.Base (HashTree (..), NodeData (..), Pr
                                                 treeModTime, treeNBytes, treeName, treeType, treeEqIgnoringModTime)
 import System.Directory.BigTrees.HashTree.Build (buildProdTree, buildTree)
 import System.Directory.BigTrees.HashTree.Edit (addSubTree, rmSubTree)
+import System.Directory.BigTrees.HashTree.Prune -- TODO specifics
 import System.Directory.BigTrees.HashTree.Find (Filter (..), listTreePaths, pathMatches)
 import System.Directory.BigTrees.HashTree.Read (accTrees, hReadTree, 
                                                 readLastHashLineAndFooter,
