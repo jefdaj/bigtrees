6bfde71444d30c277965a8a74742760b7af41b89
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-20 14:57:32 -0700

    wip Main cli

--- app/Main.hs
+++ app/Main.hs
@@ -34,69 +34,72 @@ main = do
 
   let ptns = [D.docoptFile|app/usage.txt|]
   args <- D.parseArgsOrExit ptns =<< getArgs
-  pPrint args
+  -- pPrint args
 
-  let cmd  n = D.isPresent  args $ D.command n
-      lst  n = D.getAllArgs args $ D.argument n
-      arg  n = D.getArgOrExitWith ptns args $ D.argument n
-      long n = D.getArgOrExitWith ptns args $ D.longOption n
-      mInt n = fmap (read :: String -> Int) $ D.getArg args $ D.longOption n
-      flag n = D.isPresent args $ D.longOption n
+  let cmd    n = D.isPresent  args $ D.command n
+      flag   n = D.isPresent  args $ D.longOption n
+      lstArg n = D.getAllArgs args $ D.argument n
+      reqArg n = D.getArgOrExitWith ptns args $ D.argument n
+      optArg n = D.getArg args $ D.longOption n
+      optInt n = (read :: String -> Int) <$> optArg n
 
-  eList <- case D.getArg args $ D.longOption "excludes-from" of
+  eList <- case optArg "excludes-from" of
              Nothing -> return $ excludeRegexes defaultSearchConfig
              Just f  -> readFile f <&> lines -- TODO more detailed parsing?
-  sList <- case D.getArg args $ D.longOption "searches-from" of
+
+  sList <- case optArg "searches-from" of
              Nothing -> return $ searchRegexes defaultSearchConfig
              Just f  -> readFile f <&> lines -- TODO more detailed parsing?
-  oPath <- case D.getArg args $ D.longOption "output" of
+
+  oPath <- case optArg "output" of
              Nothing -> return Nothing
              Just o  -> encodeFS o <&> Just
 
   let cfg = defaultAppConfig
         { outFile   = oPath
-        , outFormat = D.getArg args $ D.longOption "out-fmt"
+        , outFormat = optArg "out-fmt"
         , verbose   = flag "verbose"
         , searchCfg = defaultSearchConfig
-          { minBytes   = mInt "min-size"
-          , maxBytes   = mInt "max-size"
-          , minDepth   = mInt "min-depth"
-          , maxDepth   = mInt "max-depth"
-          , minFiles   = mInt "min-files"
-          , maxFiles   = mInt "max-files"
-          , minModtime = mInt "min-modtime"
-          , maxModtime = mInt "max-modtime"
+          { minBytes   = optInt "min-size"
+          , maxBytes   = optInt "max-size"
+          , minDepth   = optInt "min-depth"
+          , maxDepth   = optInt "max-depth"
+          , minFiles   = optInt "min-files"
+          , maxFiles   = optInt "max-files"
+          , minModtime = optInt "min-modtime"
+          , maxModtime = optInt "max-modtime"
           , excludeRegexes = eList
           , searchRegexes  = sList
           }
         }
+
   pPrint cfg
 
   if cmd "diff" then do
-    old <- encodeFS =<< arg "OLD"
-    new <- encodeFS =<< arg "NEW"
+    old <- encodeFS =<< reqArg "OLD"
+    new <- encodeFS =<< reqArg "NEW"
     cmdDiff cfg old new
 
   else if cmd "dupes" then do
-    hashes <- encodeFS =<< arg "HASHES"
+    hashes <- encodeFS =<< reqArg "HASHES"
     cmdDupes cfg hashes
 
   else if cmd "set-add" then do
-    set  <- encodeFS =<< arg "set"
-    let note = D.getArg args $ D.longOption "note"
-    paths <- mapM encodeFS $ lst "PATH"
+    set  <- encodeFS =<< reqArg "set"
+    let note = optArg "note"
+    paths <- mapM encodeFS $ lstArg "PATH"
     cmdSetAdd cfg set note paths
 
   else if cmd "find" then do
-    path <- encodeFS =<< arg "PATH" -- TODO multiple paths?
+    path <- encodeFS =<< reqArg "PATH" -- TODO multiple paths?
     cmdFind cfg path
 
   else if cmd "hash" then do
-     path <- encodeFS =<< arg "PATH"
+     path <- encodeFS =<< reqArg "PATH"
      cmdHash cfg path
 
   else if cmd "info" then do
-    path <- encodeFS =<< arg "PATH"
+    path <- encodeFS =<< reqArg "PATH"
     cmdInfo cfg path
 
   else if cmd "version" then
