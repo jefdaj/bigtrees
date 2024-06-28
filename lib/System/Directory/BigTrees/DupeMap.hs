cf0e435cf0e9bf7b285266f9fedd664d87bbeff3
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-27 19:27:36 -0700

    comments

--- lib/System/Directory/BigTrees/DupeMap.hs
+++ lib/System/Directory/BigTrees/DupeMap.hs
@@ -46,7 +46,6 @@ import System.Directory.BigTrees.HashLine (Depth (..), NNodes (..), TreeType (..
 import System.Directory.BigTrees.HashTree (HashTree (..), NodeData (..), ProdTree, treeType, treeHash, treeName)
 import System.Directory.BigTrees.HashTree.Search (SearchConfig (..))
 import System.Directory.BigTrees.Name (Name (..), n2op)
--- import System.OsPath (splitDirectories, (</>))
 import System.IO (Handle, IOMode (..))
 
 import Data.Functor ((<&>))
@@ -54,10 +53,6 @@ import qualified System.File.OsPath as SFO
 import System.OsPath
 
 -- TODO be able to serialize dupe tables for debugging
-
--- TODO are the paths getting messed up somewhere in here?
--- like this: myfirstdedup/home/user/bigtrees/demo/myfirstdedup/unsorted/backup/backup
-
 -- TODO can Foldable or Traversable simplify these?
 
 -- TODO is DupeSet a Monoid?
@@ -73,16 +68,6 @@ type DupeTable s = C.HashTable s Hash DupeSet
 -- TODO newtypes?
 type SortedDupeSets  = [DupeSet]
 type SortedDupeLists = [DupeList]
-
---------------------------------
--- DupeTable from hashes file --
---------------------------------
-
--- TODO is this a good idea? it would require losing the nfiles per dir thing
-
--- TODO is this the right type signature for an stToIO action?
--- dupesFromHashes :: OsPath -> DupeTable RealWorld
--- dupesFromHashes = undefined
 
 -----------------------------
 -- DupeTable from HashTree --
@@ -158,15 +143,6 @@ scoreSets = H.foldM (\vs (_, v@(_,t,fs)) ->
     -- TODO reference set case
     score (n, D, fs) = n - n `div` length fs
     score (n, _, _ ) = n - 1 -- TODO is this right?
-
--- TODO could this be faster than quicksorting everything even though single threaded?
--- usage: H.mapM_ (\(k,_) -> H.mutate dt k removeNonDupes) dt
--- rewrite of `filter hasDupes` for use with H.mutate
--- removeNonDupes :: Maybe DupeSet -> (Maybe DupeSet, ())
--- removeNonDupes Nothing = (Nothing, ())
--- removeNonDupes (Just v@(nfiles, _, paths)) = (if S.size paths > 1 && nfiles > 0
---                                                 then Just v
---                                                 else Nothing, ())
 
 {- Assumes a pre-sorted list as provided by dupesByNNodes.
  - Removes lists whose elements are all inside elements of the first list.
@@ -249,60 +225,10 @@ explainDupes md ls = mapM explainGroup ls <&> B8.unlines
       , "links would remove", B8.append (B8.pack $ show n) (disclaimer md)
       ]
 
------------------------------
--- info about copy numbers --
------------------------------
-
--- TODO remove entire section?
-
--- TODO remove? works, but not sure if ever needed
--- listAllFiles :: OsPath -> ProdTree -> [(Hash, OsPath)]
--- listAllFiles anchor (File {nodeData=(NodeData{name=Name n, hash=h})}) = [(h, anchor </> n)]
--- listAllFiles anchor (Dir {nodeData=(NodeData{name=Name n}), dirContents=cs}) =
---   concatMap (listAllFiles $ anchor </> n) cs
-
-
--- TODO rewrite allDupes by removing the subtree first then testing membership
---      (that way can use the removing part separately in cmdDedup)
-
--- helper for allDupes
--- TODO how to make the lookups safe?
--- TODO remove? works, but not sure if ever needed
--- anotherCopy :: Hash -> DupeMap -> DupeMap -> Bool
--- anotherCopy h mainMap subMap = nMain > nSub
---   where
---     (Just nMain) = (\(n,_,_) -> n) <$> M.lookup h mainMap
---     (Just nSub ) = (\(n,_,_) -> n) <$> M.lookup h subMap
-
--- TODO finish this
--- TODO remove? not sure if ever needed
---allDupes :: ProdTree -> ProdTree -> Bool
----- allDupes mainTree subTree = all safeToRmHash $ undefined subDupes
---allDupes mainTree subTree = undefined safeToRmHash $ undefined subDupes
---  where
---    mainDupes = undefined $ pathsByHash mainTree
---    subDupes  = undefined $ pathsByHash subTree
---    safeToRmHash h = anotherCopy h mainDupes subDupes
-
--- TODO remove? not sure if ever needed
--- for warning the user when their action will delete the last copy of a file
--- TODO also warn about directories, because sometimes they might care (Garageband files for example)
--- TODO make more efficient by restricting to hashes found in the removed subtree!
---      (only used for Rm right?)
---listLostFiles :: HashTree () -> HashTree () -> [OsPath]
---listLostFiles before after = filesLost
---  where
---    hashesBefore = pathsByHash before
---    hashesAfter  = pathsByHash after
---    hashesLost   = undefined hashesBefore hashesAfter
---    filesLost    = sort $ S.toList $ S.unions $ M.elems
---                 $ M.map (\(_,_,fs) -> fs)
---                 $ M.filter (\(_,t,_) -> t == F) hashesLost
-
 -----------
 -- tests --
 -----------
 
--- TODO property: if you dedup a list of the same dir 2+ times, there should only be one big overall dupe
+-- TODO property: if you dedup a list of the same dir 2+ times,
+-- there should only be one big overall dupe
 -- TODO property: adding to the dupe set should be idempotent
-
