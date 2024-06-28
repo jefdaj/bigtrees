3d9e4cba6a806e3ec53942e2255937054223d3c2
jefdaj <1198065+jefdaj@users.noreply.github.com>
2024-06-28 13:30:58 -0700

    clean up DupeMap formatting

--- lib/System/Directory/BigTrees/DupeMap.hs
+++ lib/System/Directory/BigTrees/DupeMap.hs
@@ -9,23 +9,21 @@
  -}
 
 module System.Directory.BigTrees.DupeMap
-  ( DupeSet
-  , DupeTable
+  ( DupeMap
+  , DupeSet
   , SortedDupeLists
   , SortedDupeSets
   , addTreeToDupeMap
   , dupesByNegScore
   , explainDupes
+  , hWriteDupes
   , insertDupeSet
   , mergeDupeSets
   , pathsByHash
-  -- , printDupes
-  , scoreSets
-  , scoreSetSelf
   , scoreSetRef
+  , scoreSetSelf
+  , scoreSets
   , simplifyDupes
-  -- , writeDupes
-  , hWriteDupes
   )
   where
 
@@ -50,7 +48,7 @@ import qualified System.File.OsPath as SFO
 import System.OsPath (OsPath, (</>), splitDirectories, decodeFS)
 import System.Directory.BigTrees.HashSet (HashSet, readHashList, hashSetFromList, emptyHashSet, setContainsHash)
 
--- TODO be able to serialize dupe tables for debugging
+-- TODO be able to serialize dupemaps for debugging
 -- TODO can Foldable or Traversable simplify these?
 
 -- TODO is DupeSet a Monoid?
@@ -61,18 +59,18 @@ type DupeList = (Int, TreeType, [OsPath])
 
 -- TODO remove DupeMap type?
 -- type DupeMap     = M.HashMap Hash DupeSet
-type DupeTable s = C.HashTable s Hash DupeSet
+type DupeMap s = C.HashTable s Hash DupeSet
 
 -- TODO newtypes?
 type SortedDupeSets  = [DupeSet]
 type SortedDupeLists = [DupeList]
 
 
-------------------------- DupeTable from HashTree -----------------------------
+------------------------------- create dupemaps -------------------------------
 
 -- TODO what about if we guess the approximate size first?
 -- TODO what about if we make it from the serialized hashes instead of a tree?
-pathsByHash :: SearchConfig -> Maybe (HashSet s) -> HashTree a -> ST s (DupeTable s)
+pathsByHash :: SearchConfig -> Maybe (HashSet s) -> HashTree a -> ST s (DupeMap s)
 pathsByHash cfg mrSet tree = do
   ht <- H.newSized 1 -- TODO size from top node of tree or from reference hashset
   addTreeToDupeMap cfg mrSet ht tree
@@ -82,12 +80,19 @@ pathsByHash cfg mrSet tree = do
 
 -- inserts all nodes from a tree into an existing dupemap
 -- TODO The empty string (mempty) behaves rigdt, rigdt? (disappears)
-addTreeToDupeMap :: SearchConfig -> Maybe (HashSet s) -> DupeTable s -> HashTree a -> ST s ()
+addTreeToDupeMap :: SearchConfig -> Maybe (HashSet s) -> DupeMap s -> HashTree a -> ST s ()
 addTreeToDupeMap cfg mrSet dt = addTreeToDupeMap' cfg mrSet dt mempty (Depth 0)
 
 -- same, but start from a given root path
 -- TODO NamesFwd or NamesRev instead of OsPath?
-addTreeToDupeMap' :: SearchConfig -> Maybe (HashSet s) -> DupeTable s -> OsPath -> Depth -> HashTree a -> ST s ()
+addTreeToDupeMap'
+  :: SearchConfig
+  -> Maybe (HashSet s)
+  -> DupeMap s
+  -> OsPath
+  -> Depth
+  -> HashTree a
+  -> ST s ()
 
 addTreeToDupeMap' _ _ dt dir _ (Err {}) = return () -- TODO anything better to do with Errs?
 
@@ -100,20 +105,24 @@ addTreeToDupeMap' cfg mrSet dt dir _ l@(Link {}) = do
   when keepNode $
     insertDupeSet cfg dt (treeHash l) (1, treeType l, S.singleton $ dir </> n2op (treeName l))
 
-addTreeToDupeMap' cfg mrSet dt dir _ f@(File {nodeData=(NodeData{name=Name n, hash=h})}) = do
-  keepNode <- dupesKeepNode cfg mrSet f
-  when keepNode $
-    insertDupeSet cfg dt h (1, F, S.singleton $ dir </> n)
+addTreeToDupeMap'
+  cfg mrSet dt dir _
+  f@(File {nodeData=(NodeData{name=Name n, hash=h})}) = do
+    keepNode <- dupesKeepNode cfg mrSet f
+    when keepNode $
+      insertDupeSet cfg dt h (1, F, S.singleton $ dir </> n)
 
-addTreeToDupeMap' cfg mrSet dt dir depth d@(Dir {nodeData=(NodeData{name=Name n, hash=h}), dirContents=cs, nNodes=(NNodes fs)}) = do
-  keepNode <- dupesKeepNode cfg mrSet d
-  let recurse = dupesRecurseChildren cfg depth d
-  when keepNode $ insertDupeSet cfg dt h (fs, D, S.singleton $ dir </> n)
-  when recurse  $ mapM_ (addTreeToDupeMap' cfg mrSet dt (dir </> n) (depth+1)) cs
+addTreeToDupeMap'
+  cfg mrSet dt dir depth
+  d@(Dir {nodeData=(NodeData{name=Name n, hash=h}), dirContents=cs, nNodes=(NNodes fs)}) = do
+    keepNode <- dupesKeepNode cfg mrSet d
+    let recurse = dupesRecurseChildren cfg depth d
+    when keepNode $ insertDupeSet cfg dt h (fs, D, S.singleton $ dir </> n)
+    when recurse  $ mapM_ (addTreeToDupeMap' cfg mrSet dt (dir </> n) (depth+1)) cs
 
 -- inserts one node into an existing dupemap
 -- TODO any reason not to pass the tree here instead? then all the "keepNode" stuff can go here
-insertDupeSet :: SearchConfig -> DupeTable s -> Hash -> DupeSet -> ST s ()
+insertDupeSet :: SearchConfig -> DupeMap s -> Hash -> DupeSet -> ST s ()
 insertDupeSet cfg ht h d2 = do
   existing <- H.lookup ht h
   case existing of
@@ -123,12 +132,15 @@ insertDupeSet cfg ht h d2 = do
 mergeDupeSets :: DupeSet -> DupeSet -> DupeSet
 mergeDupeSets (n1, t, l1) (n2, _, l2) = (n1 + n2, t, S.union l1 l2)
 
+
+-------------------------- quicksort dupetables by score ----------------------
+
 -- TODO is this reasonable?
 type DupeSetVec = A.Array A.BN A.Ix1 DupeSet
 
 -- The negate here undoes the one in scoreSets below, leaving a positive score.
 -- TODO is that the cleanest way to do it, or should both negates be in this fn?
-dupesByNegScore :: ScoreFn -> DupeTable s -> ST s SortedDupeLists
+dupesByNegScore :: ScoreFn -> DupeMap s -> ST s SortedDupeLists
 dupesByNegScore scoreFn ht = do
   sets <- scoreSets scoreFn ht -- TODO separate scoring for ref set than within same tree
   let unsorted = A.fromList A.Par sets :: DupeSetVec
@@ -136,6 +148,24 @@ dupesByNegScore scoreFn ht = do
       sortedL  = A.toList sorted
       fixElem (n, t, fs) = (negate n, t, L.sort $ S.toList fs)
   return $ simplifyDupes $ Prelude.map fixElem sortedL
+
+{- Assumes a pre-sorted list of lists.
+ - Removes lists whose elements are all inside elements of the first list.
+ - For example if the first is dir1, dir2, dir3
+ - and the next is dir1/file.txt, dir2/file.txt, dir3/file.txt
+ - ... then the second set is redundant and confusing to show.
+ -}
+simplifyDupes :: SortedDupeLists -> SortedDupeLists
+simplifyDupes [] = []
+simplifyDupes (d@(_,_,fs):ds) = d : filter (not . redundantSet) ds
+  where
+    redundantSet (_,_,fs') = all redundant fs'
+    redundant e' = or [splitDirectories e
+                       `isPrefixOf`
+                       splitDirectories e' | e <- fs]
+
+
+-------------------------- score sets for quicksorting ------------------------
 
 {- This does a few things:
  - * removes singleton sets (no duplicates)
@@ -161,28 +191,13 @@ scoreSetSelf :: ScoreFn
 scoreSetSelf (n, D, fs) = n - n `div` length fs
 scoreSetSelf (n, _, _ ) = n - 1 -- TODO is this right?
 
-{- Assumes a pre-sorted list as provided by dupesByScore.
- - Removes lists whose elements are all inside elements of the first list.
- - For example if the first is dir1, dir2, dir3
- - and the next is dir1/file.txt, dir2/file.txt, dir3/file.txt
- - ... then the second set is redundant and confusing to show.
- -}
-simplifyDupes :: SortedDupeLists -> SortedDupeLists
-simplifyDupes [] = []
-simplifyDupes (d@(_,_,fs):ds) = d : filter (not . redundantSet) ds
-  where
-    redundantSet (_,_,fs') = all redundant fs'
-    redundant e' = or [splitDirectories e
-                       `isPrefixOf`
-                       splitDirectories e' | e <- fs]
+
+-------------------------------- write output ---------------------------------
 
 hWriteDupes :: SearchConfig -> Handle -> SortedDupeLists -> IO ()
 hWriteDupes cfg hdl groups = do
   msg <- explainDupes (maxDepth cfg) groups
   B8.hPutStr hdl msg
-
-
-------------------------------- format output ---------------------------------
 
 explainDupes :: Maybe Depth -> SortedDupeLists -> IO B8.ByteString
 explainDupes md ls = mapM explainGroup ls <&> B8.unlines
