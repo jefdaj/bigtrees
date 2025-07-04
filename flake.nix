14df34a6968591c6589dc4b2ae10c8c42f3ba840
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-04 09:28:23 -0700

    wip update nix, stack, ghc, hspkgs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,16 @@
 {
 
+  # The current state of the code is that this flake *finally* does a proper
+  # static build, but I haven't figured out how to get it to also do a nice
+  # stack-based dev env yet. So that's still done via the old infrastrcture:
+  # shell.nix + nix/* + stack.yaml.
+
+  # TODO try current nixos-25.05 nixpkgs, but don't update hs pkgs yet
+  #        nope, gotta also update the packages
+  #        it looks promising though because docopt finally evaluates again
+  #        i think they updated default ghc -> 9.8.4 now?
+  # TODO try adding the exact logic from shell.nix as a devShell
+
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
     flake-utils.url = "github:numtide/flake-utils";
@@ -8,6 +19,7 @@
       url = github:jefdaj/directory-tree/add-no-follow-symlinks-option-again;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
   outputs = { self, nixpkgs, flake-utils, directory-tree }:
@@ -18,6 +30,7 @@
 
       let
 
+        # TODO is docopt finally fixed??
         # TODO why is docopt still blocking evaluation after markUnbroken?
         #      oh, there's a bug:
         #      https://github.com/NixOS/nixpkgs/issues/235960
@@ -74,6 +87,51 @@
           overlays = [ haskellOverlay ];
         });
 
+        # Wrap Stack to configure Nix integration and target the correct Stack-Nix file
+        # - nix: Enable Nix support
+        # - no-nix-pure: Pass environment variables, like `NIX_PATH`
+        # - nix-shell-file: Nix file to use (otherwise it uses `shell.nix` by default)
+
+        # TODO rewrite based on modern integration guide:
+        #        https://docs.haskellstack.org/en/stable/topics/nix_integration/
+        #
+        # Wrap Stack to work with our Nix integration. We don't want to modify
+        # stack.yaml so non-Nix users don't notice anything.
+        # - no-nix: We don't want Stack's way of integrating Nix.
+        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+        # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
+        stack-wrapped = pkgsDynamic.symlinkJoin {
+          name = "stack"; # will be available as the usual `stack` in terminal
+          paths = [ pkgsDynamic.stack ];
+          buildInputs = [ pkgsDynamic.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+
+        hPkgs =
+          pkgsDynamic.haskell.packages."ghc984"; # need to match Stackage LTS version
+                                                 # from stack.yaml snapshot
+
+        myDevTools = [
+          hPkgs.ghc # GHC compiler in the desired version (will be available on PATH)
+          # hPkgs.ghcid # Continuous terminal Haskell compile checker
+          # hPkgs.ormolu # Haskell formatter
+          # hPkgs.hlint # Haskell codestyle checker
+          # hPkgs.hoogle # Lookup Haskell documentation
+          # hPkgs.haskell-language-server # LSP server for editor
+          # hPkgs.implicit-hie # auto generate LSP hie.yaml file from cabal
+          # hPkgs.retrie # Haskell refactoring tool
+          # hPkgs.cabal-install
+          stack-wrapped
+          pkgsDynamic.zlib # External C library needed by some Haskell packages
+        ];
+
       # Static by default, but allow pkgsDynamic to be referenced explicitly for dev tools.
       in with pkgsDynamic.pkgsStatic;
       let
@@ -123,26 +181,32 @@
         ];
 
         # The dev tools could probably also be static, but why rebuild them?
-        devShell = project (executableSystemDepends ++ [
+        # devShells.default = pkgs.mkShell {
+        devShell = pkgsDynamic.mkShell {
+          buildInputs = myDevTools;
 
+          # Make external Nix c libraries like zlib known to GHC, like
+          # pkgs.haskell.lib.buildStackProject does
+          # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
+          LD_LIBRARY_PATH = pkgsDynamic.lib.makeLibraryPath myDevTools;
+        };
+
+        # devShell = project (executableSystemDepends ++ [
           # TODO *any* package here evaluates the broken docopt? weird
           # hello
-
           # cabal-fmt
           # cabal-install TODO why does this fail on perl?
           # haskell-language-server
           # hlint
-
           # test
           # pkgsDynamic.tree
-
           # analyze/lint
           # pkgsDynamic.hlint
           # pkgsDynamic.haskellPackages.apply-refact
           # stylish-haskell
           # pkgsDynamic.haskellPackages.weeder
           # pkgsDynamic.haskellPackages.stan
+        # ]);
 
-        ]);
       });
 }
