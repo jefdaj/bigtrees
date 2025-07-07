3f8c031027de36bf0ff29792e53f05391c2f3229
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-06 21:07:38 -0700

    download.rst -> .md

--- docs/src/site.hs
+++ docs/src/site.hs
@@ -22,7 +22,7 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["download.rst", "questions.md"]) $ do
+    match (fromList ["download.md", "questions.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
