67058d1a1b6ed49a1054027fc22201292a3bccf0
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-13 18:32:26 -0700

    fix double path in rsync exclude file dupesets

--- lib/System/Directory/BigTrees/DupeMap.hs
+++ lib/System/Directory/BigTrees/DupeMap.hs
@@ -363,8 +363,8 @@ renderDupesRsyncExclude keepOne md ls = do
       paths' <- mapM decodeFS paths -- TODO is decoding necessary, even to write a script?
       let paths''  = sortPaths $ map (escapeRsyncExcludeFromPath2 . replaceTopDirWithSlash) paths'
           paths''' = if not keepOne
-                       then map ("-" ++) $ paths''
-                       else ("+" ++ head paths''):(map ("-" ++) $ tail paths'')
+                       then paths''
+                       else ("# " ++ head paths''):(tail paths'')
       return $ B8.unlines $ groupHeader t n (length paths) : map B8.pack paths'''
 
     nSkip ds = B8.pack $ show $ if keepOne then ds - 1 else ds
