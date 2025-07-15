f5d7fa4c24bc71f2328d1b4f8d09513dd07a98a5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-07-14 22:50:48 -0700

    faster simplifyDupes by skipping non-dir iterations

--- lib/System/Directory/BigTrees/DupeMap.hs
+++ lib/System/Directory/BigTrees/DupeMap.hs
@@ -164,7 +164,7 @@ dupesByNegScore mLog scoreFn ht = do
       sorted   = debug "quicksorting DupeSetVec" $ A.quicksort $ A.compute unsorted :: DupeSetVec
       sortedL  = debug "converting DupeSetVec back to list" $ A.toList sorted
       fixElem (n, t, fs) = (negate n, t, L.sort $ S.toList fs)
-      simple = debug "simplifying dupes" $ simplifyDupes 0 mLog $ Prelude.map fixElem sortedL
+      simple = debug "simplifying dupes" $ simplifyDupes 1 mLog $ Prelude.map fixElem sortedL
   return simple
 
 {- Assumes a pre-sorted list of lists.
@@ -174,17 +174,25 @@ dupesByNegScore mLog scoreFn ht = do
  - ... then the second set is redundant and confusing to show.
  -}
 simplifyDupes :: Int -> Maybe LogFn -> SortedDupeLists -> SortedDupeLists
-simplifyDupes _ _ [] = []
-simplifyDupes i mLog (d@(_,_,fs):ds) =
-  debug ("iteration " <> B8.pack (show i)) $
-  (d:) $ simplifyDupes (i+1) mLog $ filter (not . redundantSet) ds
+
+simplifyDupes _ _ [ ] = [ ]
+simplifyDupes _ _ [d] = [d]
+
+simplifyDupes i mLog (d@(_,D,fs):ds) =
+  debug ("removed " <> B8.pack (show nSaved) <>
+         " DupeSets redundant with set #" <> B8.pack (show i)) $
+  (d:) $ simplifyDupes (i+1) mLog $ ds'
   where
+    ds' = filter (not . redundantSet) ds
+    nSaved = length ds - length ds'
     debug = logMaybeUnsafe mLog DebugL "simplifyDupes"
     redundantSet (_,_,fs') = all redundant fs'
     redundant e' = or [splitDirectories e
                        `L.isPrefixOf`
                        splitDirectories e' | e <- fs]
 
+-- TODO double check that these can't have redundancies
+simplifyDupes i mLog (d@(_,_,_):ds) = (d:) $ simplifyDupes (i+1) mLog $ ds
 
 ---------------------------- pick which dupe to keep --------------------------
 
