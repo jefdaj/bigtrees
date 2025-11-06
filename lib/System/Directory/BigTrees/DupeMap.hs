6e4a6c752e5c29ba3e748445c3e3ac310a890919
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-11-05 18:15:35 -0800

    add newest modtime as the first dupe sort criteria

--- lib/System/Directory/BigTrees/DupeMap.hs
+++ lib/System/Directory/BigTrees/DupeMap.hs
@@ -287,10 +287,12 @@ redundantSet lCfg h1 fs (_,h2,_,fs') =
 -- TODO move to a util module
 
 -- Compare paths according to my (idiosyncratic) intuition so far:
+-- 0. newer modtime first
 -- 1. non-hidden files first
 -- 2. fewer path components first
 -- 3. shorter names first
 -- 4. alphabetically as usual
+-- TODO make this customizable with string descriptions in the config
 comparePaths :: ModPath -> ModPath -> Ordering
 comparePaths (ma, a) (mb, b) =
 
@@ -302,14 +304,16 @@ comparePaths (ma, a) (mb, b) =
       isHiddenPath p = any startsWithDot $ LS.splitOn "/" p
       countComponents path = length (LS.splitOn "/" path)
 
-  in case (isHiddenPath a', isHiddenPath b') of
-       (True, False) -> GT
-       (False, True) -> LT
-       _ -> case comparing countComponents a' b' of
-              EQ -> case comparing length a' b' of
-                      EQ  -> compare a' b'
+  in case comparing (0-) ma mb of
+       EQ -> case (isHiddenPath a', isHiddenPath b') of
+               (True, False) -> GT
+               (False, True) -> LT
+               _ -> case comparing countComponents a' b' of
+                      EQ -> case comparing length a' b' of
+                              EQ  -> compare a' b'
+                              ord -> ord
                       ord -> ord
-              ord -> ord
+       ord -> ord
 
 -- TODO this probably needs to be OsPaths, right?
 --      maybe keep the original paths, but decorate with string versions for sorting?
