6f663f0cbf617bb116aa1bc481463d53244228b8
jefdaj <1198065+jefdaj@users.noreply.github.com>
2025-11-16 07:50:55 -0800

    test.sh comments

--- test.sh
+++ test.sh
@@ -2,38 +2,42 @@
 
 # Usage: ./test.sh
 #
-# No need to be in a Nix shell first.
+# No need to be in a Nix shell first; each step below handles that.
 # Comment out steps and adjust env vars as needed during development,
 # or use it as a guide and just run stack test or bats manually.
 
 # TODO move golden tests to BATS?
 # TODO why aren't the doctests being run?
-# TODO when/how to run benchmarks?
 
-# set -x
 set -e
 
 echo "### TESTING STATIC BINARY WITH BATS ###"
-echo "note that downloading 72M test-files might take a few minutes..."
-# Useful for debugging:
+
+nix build
+
+# How to debug bats examples:
+#
 #   BATSLIB_TEMP_PRESERVE=1 bats ...
 #   sudo find /tmp | grep bats
-nix build
+
 nix develop -c bash -c 'bats test/bats'
 
 echo "### RUNNING HASKELL TEST SUITE ###"
 
-# Some useful env vars:
+# Some useful TASTY_ vars you can add:
 #
 # TASTY_QUICKCHECK_TESTS=10000
 # TASTY_PATTERN="/ByteString/"
+
 nix develop -c bash -c '
   TASTY_QUICKCHECK_TESTS=1000 \
   stack test bigtrees
 '
 
-# OK to inturrupt if you get bored at this point...
 echo "### RUNNING HASKELL BENCHMARKS ###"
+
+# OK to inturrupt it if you get bored at this point
+
 nix develop -c bash -c '
   stack bench bigtrees
 '
